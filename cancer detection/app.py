7ad10811eef42646fa76904c2fbcbd25d1c0312f
nithin7764 <nithinrockerr00@gmail.com>
2025-07-27 20:46:55 +0530

    Add files via upload

---
+++ cancer detection/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load('model.pkl')
+
+# List of features in the order your model expects
+FEATURES = [
+    'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean',
+    'compactness_mean', 'concavity_mean', 'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
+    'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se',
+    'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se', 'fractal_dimension_se',
+    'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst',
+    'compactness_worst', 'concavity_worst', 'concave points_worst', 'symmetry_worst', 'fractal_dimension_worst'
+]
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    # Extract features in the correct order
+    features = [data[feature] for feature in FEATURES]
+    features = np.array(features).reshape(1, -1)
+    prediction = model.predict(features)
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
